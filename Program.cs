c73119095007c656eccfec6785d21c983c10cc0e
TC_win_laptop <88120195+TcPirate1@users.noreply.github.com>
2023-07-27 10:22:02 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+MongoClient client = new("ATLAS_URI_HERE");
+Console.WriteLine("Hello, World!");
