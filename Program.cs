36d4a4c1885547ada9b41015594395bc9c7910a9
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2023-08-03 21:34:03 +1200

    Removed test solution. Nuget package to read .env file and reading of said file changed to root rather than /bin/debug

--- Program.cs
+++ Program.cs
@@ -2,6 +2,34 @@
 
 using MongoDB.Driver;
 using MongoDB.Bson;
+using DotNetEnv;
 
-MongoClient client = new("ATLAS_URI_HERE");
-Console.WriteLine("Hello, World!");
+string projectRoot = GetProjectRoot();
+Environment.CurrentDirectory = projectRoot;
+
+Env.Load();
+
+string dbCluster = Environment.GetEnvironmentVariable("ATLAS_URI");
+
+MongoClient client = new(dbCluster);
+
+List<string> databases = client.ListDatabaseNames().ToList();
+
+foreach (string database in databases)
+{
+    Console.WriteLine(database);
+}
+
+static string GetProjectRoot()
+{
+    string currentDirectory = Directory.GetCurrentDirectory();
+    DirectoryInfo directory = new(currentDirectory);
+
+    // Traverse up the directory tree until the solution file (e.g., .sln) is found
+    while (directory != null && !directory.GetFiles("*.sln").Any())
+    {
+        directory = directory.Parent;
+    }
+
+    return directory?.FullName;
+}
