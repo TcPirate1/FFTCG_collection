495c48446fd0888cc6f54dc3ca18bc90e75390c5
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2023-09-04 22:08:38 +1200

    Added copies to the Card class and added validation checks for cost and copies

--- Card.cs
+++ Card.cs
@@ -14,13 +14,14 @@ namespace FFTCG_collection
         private string Name { get; set; }
         private string Image { get; set; }
         private string Type { get; set; }
-        private double Cost { get; set; }
+        private int Cost { get; set; }
         private string[] Special_icons { get; set; }
         private string[] Elements { get; set; }
         private string Code { get; set; }
+        private int Copies { get; set; }
         private bool Foil { get; set; }
 
-        public Card(string name, string image, string type, double cost, string[] special_icons, string[] elements, string code, bool foil)
+        public Card(string name, string image, string type, int cost, string[] special_icons, string[] elements, string code, int copies, bool foil)
         {
             Name = name;
             Image = image;
@@ -29,11 +30,13 @@ namespace FFTCG_collection
             Special_icons = special_icons;
             Elements = elements;
             Code = code;
+            Copies = copies;
             Foil = foil;
         }
 
         public static BsonDocument CardAdd()
         {
+            string input;
             Console.WriteLine("Name of card: ");
             string cardname = Console.ReadLine()!.Trim();
             Console.WriteLine("Image location: ");
@@ -41,7 +44,12 @@ namespace FFTCG_collection
             Console.WriteLine("What is the card's type?");
             string type = Console.ReadLine()!.Trim();
             Console.WriteLine("What is the card's cost?");
-            double cost = Convert.ToDouble(Console.ReadLine()!.Trim());
+            int cost;
+            input = Console.ReadLine()!.Trim();
+            while (int.TryParse(input, out cost) != true || cost < 1 || cost > 11)
+            {
+                Console.WriteLine("Invalid cost or you inputted a number lower than 1 and higher than 11.\nThere is currently no higher cost than 11.\n Please input a number.");
+            }
             Console.WriteLine("What is the card's special icons?\nEnter with spaces please.\n");
             string icons = Console.ReadLine()!.Trim();
             string[] iconsArray = icons.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
@@ -53,7 +61,15 @@ namespace FFTCG_collection
             while (CardRegex(code) != true)
             {
                 Console.WriteLine("Please enter a valid card code.");
-                code = Console.ReadLine()!;
+                code = Console.ReadLine()!.Trim().ToUpper();
+            }
+            Console.WriteLine("How many copies are there?");
+            int copies;
+            input = Console.ReadLine()!.Trim();
+            while (int.TryParse(input, out copies) != true || copies == 0)
+            {
+                Console.WriteLine("Invalid number or 0 was inputted.\nThere shouldn't be 0 copies of a card in the collection.\nPlease enter a valid number.");
+                input = Console.ReadLine()!.Trim();
             }
             Console.WriteLine("Is this card a foil?\nEnter \'y\' for yes and \'n\' for no.");
             char foilValid = Convert.ToChar(Console.ReadLine()!.Trim().ToLower());
@@ -64,7 +80,7 @@ namespace FFTCG_collection
             }
             bool foil = foilValid == 'y' ? true : false;
 
-            Card newCard = new(cardname, image, type, cost, iconsArray, elementsArray, code, foil);
+            Card newCard = new(cardname, image, type, cost, iconsArray, elementsArray, code, copies, foil);
 
             var newDocument = new BsonDocument
             {
@@ -75,6 +91,7 @@ namespace FFTCG_collection
                 { "Special Icons", new BsonArray(newCard.Special_icons)},
                 { "Elements", new BsonArray(newCard.Elements)},
                 { "Card code", newCard.Code},
+                { "Copies", newCard.Copies},
                 { "Foil?", newCard.Foil}
 
             };
